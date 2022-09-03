08ab2afff5c7bc3b0e81030565ea93eae69f829f
bichri <hamzabichri1997@gmail.com>
2022-09-03 17:53:48 +0100

    first commit

--- src/pages/posts/Posts.js
+++ src/pages/posts/Posts.js
@@ -2,9 +2,51 @@
 import React from 'react';
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import Popup from 'reactjs-popup';
 import './Posts.css'
  
 function Posts() {
+
+  const [data ,setData] = useState({});
+
+  const PopupExample = (id) => (
+    
+<>
+    {
+      fetch(`https://jsonplaceholder.typicode.com/comments/${id.name}`)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setData(result);
+        })
+
+    }
+
+
+    <Popup
+       trigger={<button className="button"> Show </button>}
+    modal
+    nested
+  >
+    {close => (
+      <div className="modal">
+        <button className="close" onClick={close}>
+          &times;
+        </button>
+        <div className="header">  {data.name} </div>
+        <div className="content">
+
+           {data.body}
+            
+        </div>
+       
+      </div>
+    ) }
+
+  </Popup>
+  </>
+  );
+
 
     const navigate = useNavigate();
     const [dataPost,setDataPost] = useState();
@@ -15,7 +57,7 @@ function Posts() {
         // verfier connexion et fetch data 
         if (loggedInUser) {
 
-            fetch("https://jsonplaceholder.typicode.com/posts")
+            fetch("https://jsonplaceholder.typicode.com/users")
             .then(res => res.json())
             .then(
               (result) => {
@@ -28,17 +70,15 @@ function Posts() {
       }, []);
 
 
-
+ 
   return (
 
 
     <div className="Posts">
-
-         <h1>Posts</h1>
          
          <div className='container'>
            
-         <div class="grid-container">
+         <div className="grid-container">
  
          {dataPost ? 
             
@@ -57,6 +97,67 @@ function Posts() {
 
 
          </div>
+
+<section>
+
+<div className="tbl-header">
+  <table >
+    <thead>
+      <tr>
+        <th>id</th>
+        <th>name</th>
+        <th>username</th>
+        <th>email</th>
+        <th>details</th>
+      </tr>
+    </thead>
+  </table>
+</div>
+<div className="tbl-content">
+  <table >
+    <tbody>
+
+    {dataPost ? 
+            
+            dataPost.map((data, index) =>
+             
+            <tr  key={index}>
+            <td>{data.id}</td>
+            <td>{data.name} </td>
+            <td>{data.username}</td>
+            <td>{data.email}</td>
+             {/* passe id props */}
+            <td><a className='show'><PopupExample name={data.id} /></a> </td>
+            </tr>
+ 
+            )
+              
+            : 'lodding...'}
+
+
+     
+  
+    </tbody>
+  </table>
+</div>
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     </div>
   );
 }
